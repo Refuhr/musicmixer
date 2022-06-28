da6000c91d10766d41577ed2730a7d1104bd088c
Refuhr <moritz.ruffer@gmail.com>
2022-06-28 23:02:06 +0200

    added volume control + ascii art banner

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ from pygame import mixer
 
 
 
-path="music"        #path/directory where the songs are located
-fade_in=1000        #fade in time in ms
-fade_out=3000       #fade out time in ms
+path="music"                #path/directory where the songs are located
+fade_in=1000                #fade in time in ms
+fade_out=3000               #fade out time in ms
+volume=0.2                  #change the volume from 0.0 to 1.0
 
 files = os.listdir(path)
 files.sort()
 print(f"{files}\n")
 index=0
 mixer.init()
+mixer.music.set_volume(volume)
 
 def play_song():
     mixer.music.load('music/' + files[index])
     print(f"Now playing: {files[index]}")
     mixer.music.play(start=int(files[index][:len(files[index])-4].split("-")[1]), fade_ms=fade_in)
 
+def check_float(num):
+    try:
+        float(num)
+        volume=float(num)
+        mixer.music.set_volume(volume)
+        print(f"Setting volume to: {mixer.music.get_volume()}.")
+    except ValueError:
+        print("Not a float try again!")
 
 print(f"About to play: {files[index]}\n")
 print("Press any key to start the music script")
@@ -26,8 +36,15 @@ play_song()
 
 
 while True: 
-  
-    print("\nPress 'n' to play the next song, 'p' to play previous song, 'o' to manually overwrite to a specific song, 's' to stop/pause the music, 'r' to resume the music\n") 
+    print("""\
+    
+███    ███ ██    ██ ███████ ██  ██████ ███    ███ ██ ██   ██ ███████ ██████  
+████  ████ ██    ██ ██      ██ ██      ████  ████ ██  ██ ██  ██      ██   ██ 
+██ ████ ██ ██    ██ ███████ ██ ██      ██ ████ ██ ██   ███   █████   ██████  
+██  ██  ██ ██    ██      ██ ██ ██      ██  ██  ██ ██  ██ ██  ██      ██   ██ 
+██      ██  ██████  ███████ ██  ██████ ██      ██ ██ ██   ██ ███████ ██   ██ 
+""")
+    print("Press 'n' to play the next song, 'p' to play previous song, 'o' to manually overwrite to a specific song, 's' to stop/pause the music, 'r' to resume the music, v to change the volume") 
     print("Press 'e' to exit the program") 
     query = input("  ") 
   
@@ -75,4 +92,8 @@ while True:
         mixer.music.pause() 
     elif query == 'r': 
         # Resume the music
-        mixer.music.unpause() 
+        mixer.music.unpause()
+    elif query == 'v': 
+        # Change the volume
+        print("Type a number from 0.0 to 1.0")
+        check_float(input())
