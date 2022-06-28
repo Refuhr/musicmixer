294fccbc2efe93cc095d541addbf7a80b9d36cee
Refuhr <moritz.ruffer@gmail.com>
2022-06-28 19:27:19 +0200

    first gui

--- main.py
+++ main.py
@@ -4,13 +4,31 @@ path="music"
 files = os.listdir(path)
 print(files)
 index=0
-os.system("pause")
 mixer.init()
 mixer.music.load('music/' + files[index])
+print("Press any key to start the music script")
+os.system("pause >nul")
 mixer.music.play(start=31)
-os.system("pause")
-mixer.music.fadeout(3000)
-index += 1
-mixer.music.load('music/' + files[index])
-mixer.music.play()
-os.system("pause")
+
+while True: 
+  
+    print("Press 'n' to play the next song, 'p' to play previous song") 
+    print("Press 'e' to exit the program") 
+    query = input("  ") 
+  
+    if query == 'n': 
+        # Playing next song
+        mixer.music.fadeout(3000)
+        index += 1
+        mixer.music.load('music/' + files[index])
+        mixer.music.play()        
+    elif query == 'p': 
+        # Playing previous song
+        mixer.music.stop()
+        index -= 1
+        mixer.music.load('music/' + files[index])
+        mixer.music.play() 
+    elif query == 'e': 
+        # Stop the mixer 
+        mixer.music.stop() 
+        exit()
