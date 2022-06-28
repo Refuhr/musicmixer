a5ab48848d0ae49e4a63409f6956c284cfd5f8da
Refuhr <moritz.ruffer@gmail.com>
2022-06-28 19:14:38 +0200

    added queue system + another test song

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
 from pygame import mixer
+path="music"
+files = os.listdir(path)
+print(files)
+index=0
+os.system("pause")
 mixer.init()
-mixer.music.load('music/test.mp3')
+mixer.music.load('music/' + files[index])
 mixer.music.play(start=31)
-print("End")
 os.system("pause")
 mixer.music.fadeout(3000)
+index += 1
+mixer.music.load('music/' + files[index])
+mixer.music.play()
 os.system("pause")
