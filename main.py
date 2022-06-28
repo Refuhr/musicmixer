4614ba98d7e396c5776237625bb893310fd26be5
Refuhr <moritz.ruffer@gmail.com>
2022-06-28 21:25:21 +0200

    code cleanup + added start/stop function

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import os
 from pygame import mixer
+
+
+
 path="music"
 files = os.listdir(path)
-print(files)
+print(f"{files}\n")
 index=0
 mixer.init()
-mixer.music.load('music/' + files[index])
-print(f"About to play: {files[index]}")
+
+def play_song():
+    mixer.music.load('music/' + files[index])
+    print(f"Now playing: {files[index]}")
+    mixer.music.play(start=int(files[index][:len(files[index])-4].split("-")[1]))
+
+
+print(f"About to play: {files[index]}\n")
 print("Press any key to start the music script")
 os.system("pause >nul")
-print(f"Now playing: {files[index]}")
-mixer.music.play(start=int(files[index][:len(files[index])-4].split("-")[1]))
+play_song()
+
 
 while True: 
   
-    print("Press 'n' to play the next song, 'p' to play previous song, 'o' to manually overwrite to a specific song") 
+    print("\nPress 'n' to play the next song, 'p' to play previous song, 'o' to manually overwrite to a specific song, 's' to stop/pause the music, 'r' to resume the music\n") 
     print("Press 'e' to exit the program") 
     query = input("  ") 
   
@@ -26,16 +35,12 @@ while True:
             mixer.music.fadeout(3000)
             os.system("pause")
             index += 1
-            mixer.music.load('music/' + files[index])
-            print(f"Now playing: {files[index]}")
-            mixer.music.play(start=int(files[index][:len(files[index])-4].split("-")[1]))        
+            play_song()       
     elif query == 'p': 
         # Playing previous song
-        mixer.music.stop()
+        mixer.music.fadeout(500)
         index -= 1
-        mixer.music.load('music/' + files[index])
-        print(f"Now playing: {files[index]}")
-        mixer.music.play(start=int(files[index][:len(files[index])-4].split("-")[1])) 
+        play_song()
     elif query == 'o': 
         # Jump to specific song
         print("Type in the lastname: ")
@@ -51,10 +56,14 @@ while True:
         else:
             print(f"Switching to '{files[index]}'.")
             mixer.music.fadeout(500)
-            mixer.music.load('music/' + files[index])
-            print(f"Now playing: {files[index]}")
-            mixer.music.play(start=int(files[index][:len(files[index])-4].split("-")[1]))
+            play_song()
     elif query == 'e': 
         # Stop the mixer 
         mixer.music.stop() 
         exit()
+    elif query == 's': 
+        # Pause the music 
+        mixer.music.pause() 
+    elif query == 'r': 
+        # Resume the music
+        mixer.music.unpause() 
