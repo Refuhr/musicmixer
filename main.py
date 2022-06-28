4184fd5a46be8bcac0116a819273497b514de4ea
Refuhr <moritz.ruffer@gmail.com>
2022-06-28 22:28:51 +0200

    code cleanup + added fade in/out variables + error detection for first song in queue

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from pygame import mixer
 
 
 
-path="music"
+path="music"        #path/directory where the songs are located
+fade_in=1000        #fade in time in ms
+fade_out=3000       #fade out time in ms
+
 files = os.listdir(path)
-#print(f"{files}\n")
 files.sort()
 print(f"{files}\n")
 index=0
@@ -14,7 +16,7 @@ mixer.init()
 def play_song():
     mixer.music.load('music/' + files[index])
     print(f"Now playing: {files[index]}")
-    mixer.music.play(start=int(files[index][:len(files[index])-4].split("-")[1]))
+    mixer.music.play(start=int(files[index][:len(files[index])-4].split("-")[1]), fade_ms=fade_in)
 
 
 print(f"About to play: {files[index]}\n")
@@ -34,7 +36,7 @@ while True:
         if index+1 >= len(files):
             print("Reached the end!")
         else:
-            mixer.music.fadeout(3000)
+            mixer.music.fadeout(fade_out)
             index += 1
             print(f"About to play: {files[index]}\n")
             print("Press any key to continue...")
@@ -42,9 +44,12 @@ while True:
             play_song()       
     elif query == 'p': 
         # Playing previous song
-        mixer.music.fadeout(500)
-        index -= 1
-        play_song()
+        if index-1 < 0:
+            print("There is no song before that!")
+        else:
+            mixer.music.fadeout(500)
+            index -= 1
+            play_song()
     elif query == 'o': 
         # Jump to specific song
         print("Type in the lastname: ")
