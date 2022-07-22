235e247d7ceac05a36c41eb64e585d5a08817270
Refuhr <moritz.ruffer@gmail.com>
2022-07-22 19:40:27 +0200

    added manual jump by firstname, number + when skipping song still in main loop

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pygame import mixer
 
 
 
-path="music/"                #path/directory where the songs are located
+path="music2/"               #path/directory where the songs are located
 fade_in=1000                #fade in time in ms
 fade_out=3000               #fade out time in ms
 volume=0.1                  #change the volume from 0.0 to 1.0
@@ -36,8 +36,12 @@ try:
 except FileNotFoundError:
     print("Directory could not be found! Exiting...")
     exit()
+# Variables
 index=0
 exists = False
+skip = False
+found = False
+
 mixer.init()
 mixer.music.set_volume(volume)
 
@@ -45,7 +49,7 @@ def play_song():
     mixer.music.stop()
     mixer.music.load(path + files[index])
     print(f"Now playing: {files[index]}")
-    mixer.music.play(start=int(files[index][:len(files[index])-4].split("-")[1]), fade_ms=fade_in)
+    mixer.music.play(start=int(files[index][:len(files[index])-4].split("_")[3]), fade_ms=fade_in)
 
 def check_float(num):
     try:
@@ -61,19 +65,21 @@ def check_float(num):
 while True: 
     print("\nPress h for Help, e for exit.")
     if index+1 >= len(files):
-        y = ""
+        x = ""
+    elif skip:
+        x = f"About to play: {files[index]}"
     else:
-        y = "Next song: " + files[index+1]
-    if index > 0 and y != "":
-        x = ", previous song: " + files[index-1]
-    elif index > 0 and y == "":
-        x = "Previous song: " + files[index-1]
+        x = "Next song: " + files[index+1]
+    if index > 0 and x != "":
+        y = ", previous song: " + files[index-1]
+    elif index > 0 and x == "":
+        y = "Previous song: " + files[index-1]
     elif index == 0 and not exists:
         help()
-        x = "\nPress 'enter' to start with the first song: " + files[index] + " 'n' to start with second one: "  + files[index+1] + ", 'e' to exit."
+        y = "\nPress 'enter' to start with the first song: " + files[index] + " 'n' to start with second one: "  + files[index+1] + ", 'e' to exit."
     else:
-        x = ""
-    print(y+x)
+        y = ""
+    print(x+y)
     if exists: print(f'Current song: {files[index]} {"(paused)" if not mixer.music.get_busy() else ""}')
     query = input() 
   
@@ -85,19 +91,23 @@ while True:
         if index+1 >= len(files):
             print("Reached the end!")
             mixer.music.fadeout(fade_out)
+        elif skip:
+            #print("Press enter to start the next song.")
+            #input()
+            skip = False
+            play_song()
         else:
             if mixer.music.get_busy():
                 mixer.music.fadeout(fade_out)
+                print('Fading out...')
                 index += 1
-                print(f"About to play: {files[index]}\n")
-                print("Press enter to start the next song.")
-                input()
-                play_song()
+                skip = True
             else:
                 index += 1
                 play_song()
     elif query == 'l': 
         # Playing the last song
+        skip = False
         exists = True
         if index-1 < 0:
             print("There is no song before that! Restarting current song...")
@@ -108,17 +118,30 @@ while True:
             play_song()
     elif query == 'm': 
         # manually jump to specific song
-        print("Type in the lastname: ")
+        skip = False
+        print("Type in the lastname or firstname or song number (lower or uppercase doesn't matter): ")
         overwrite = input()
-        index_old = index
-        for i in files:
-            if i[4:-1].split("_")[0] == overwrite:
-                print(i)
-                index = files.index(i)
-        if index_old == index:
-            print(f"The name: '{overwrite}' could not be found!")
-            
+        try:
+            overwrite = int(overwrite)
+            for i in files:
+                if overwrite == int(i[0:3]):
+                    index = files.index(i)
+                    found = True
+        except ValueError:
+            for i in files:
+                if i[4:-1].split("_")[0].lower() == overwrite.lower():
+                    print(i)
+                    index = files.index(i)
+                    found = True
+                if i[4:-1].split("_")[1].lower() == overwrite.lower(): #if there are people having the same name, one as firstname the other one lastname, it would only choose the one with the lastname
+                    print(i)
+                    index = files.index(i)
+                    found = True
+        if not found:
+            print(f"The name, number: '{overwrite}' could not be found!")
+                
         else:
+            found = False
             exists = True
             print(f"Switching to '{files[index]}'.")
             mixer.music.fadeout(500)
